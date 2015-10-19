7a523f955eb3fc557c485b947f243712278ab724
Erin Marchak <erin@emarchak.com>
2015-10-19 18:38:55 -0400

    Entity field query and list view for GalleryController

--- modules/custom/photography/src/Controller/GalleryController.php
+++ modules/custom/photography/src/Controller/GalleryController.php
@@ -15,17 +15,65 @@ use Drupal\Core\Controller\ControllerBase;
  * @package Drupal\photography\Controller
  */
 class GalleryController extends ControllerBase {
+
   /**
-   * Gallery.
+   * Helper function to load all published photographs.
    *
-   * @return string
-   *   Return Hello string.
+   * @param string $bundle_type
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   Array of node objects keyed by nid.
    */
-  public function gallery() {
-    return [
-        '#type' => 'markup',
-        '#markup' => $this->t('Implement method: gallery')
-    ];
+  protected function loadAllPhotos($bundle_type = 'photograph') {
+    // Return the entity manager service and load the the storage instance for nodes.
+    // That way we have access to the enity api while keeping our controller lean.
+    $storage = $this->entityManager()->getStorage('node');
+
+    // Perform the entity query, as per usual.
+    $nids = $storage->getQuery()
+      ->condition('type', $bundle_type)
+      ->condition('status', 1)
+      ->execute();
+
+    // Load the nids we found.
+    return $storage->loadMultiple($nids);
   }
 
+  /**
+   * Gallery display.
+   *
+   * @return string
+   *   Return Gallery output string.
+   */
+  public function gallery() {
+
+    // Load the language manager service, so we can parse the user's current language.
+    $langcode    = $this->languageManager()->getCurrentLanguage('language');
+
+    $photographs = $this->loadAllPhotos();
+    $view_mode   = 'teaser';
+    $gallery     = array();
+
+    // Loop through the loaded photograph node objects and output their rendered result into a list.
+    foreach ($photographs as $photograph) {
+      $render    = node_view($photograph, $view_mode, $langcode);
+      $gallery[] = render($render);
+    }
+
+    // If the gallery is empty, we should apologise.
+    if (empty($gallery)) {
+      return [
+        '#type' => 'markup',
+        '#markup' => $this->t('Sorry, I have no photographs to share at the moment'),
+        '#prefix' => '<h2>',
+        '#suffix' => '</h2>',
+      ];
+    }
+
+    // Return an item list of photographs for our gallery.
+    return [
+      '#theme' => 'item_list',
+      '#items' => $gallery,
+    ];
+
+  }
 }
