f0938ca50f6072a7a0de92e4531f65435fbe6dec
Erin Marchak <erin@emarchak.com>
2015-10-21 17:48:52 -0400

    Added unit tests

---
+++ modules/photography/Tests/OrderFormTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\photography\Tests\Unit\OrderFormTest.
+ */
+
+namespace Drupal\Tests\photography\Unit\OrderFormTest;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\photography\Form\OrderForm;
+
+/**
+ * Tests validation of postal codes.
+ *
+ * @group photography
+ */
+class OrderFormTest extends UnitTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => "Photography OrderFormTest's tests",
+      'description' => 'Test Unit for form OrderForm.',
+      'group' => 'photography',
+    );
+  }
+
+  /**
+   * Get an accessible method using reflection.
+   */
+  protected function getAccessibleMethod($class_name, $method_name) {
+    $class = new \ReflectionClass($class_name);
+    $method = $class->getMethod($method_name);
+    $method->setAccessible(TRUE);
+    return $method;
+  }
+
+  protected static function getValidPostalCode() {
+    return 'M6G 2S5';
+  }
+  protected static function getInvalidPostalCode() {
+    return '90210';
+  }
+  /**
+   * Test Valid postal codes.
+   */
+  public function testPostalCodeValid() {
+    $postalCodeValidator = $this->getAccessibleMethod(
+      'Drupal\product\Form\ProductRequestForm',
+      'validPostalCode'
+    );
+    $form = new OrderForm();
+    $postal_code = $this->getValidPostalCode();
+    $this->assertTrue($postalCodeValidator->invokeArgs($form, array($postal_code)));
+  }
+  /**
+   * Test invalid postal codes.
+   */
+  public function testPostalCodeInvalid() {
+    $postalCodeValidator = $this->getAccessibleMethod(
+      'Drupal\product\Form\ProductRequestForm',
+      'validPostalCode'
+    );
+    $form = new OrderForm();
+    $postal_code = $this->getInvalidPostalCode();
+    $this->assertFALSE($postalCodeValidator->invokeArgs($form, array($postal_code)));
+  }
+}
